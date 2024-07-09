d119144c93962dc64bf02d314fc4aa1a6da1db2b
callor <callor@callor.com>
2024-07-09 16:24:28 +0900

    화면구현

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # UIUX 화면구현 2024
+
+# VS Code 설치
+
+- 검색창에 `vsCode` 를 입력하여 `visual Code Studio` 를 찾아 다운로드 및 설치하기
+
+## vsCode 실행하기
+
+- 작업폴더에서 마우스 우클릭하여 vsCode 실행하기
+
+## vsCode 플러그인 설치하기
+
+![alt text](./images/image-1.png)
+![alt text](./images/image-2.png)
+
+- 검색 창에 `Korea` 입력 : `Korea Lan. Pack` 설치
+
+![alt text](./images/image-3.png)
+![alt text](./images/image-4.png)
+
+- 검색창에 `prettier` 입력 : `Prettier - Code formatter` 설치
+
+![alt text](./images/image-5.png)
+
+- 검색창에 `EsLint` 입력 : `EsLint` 설치
+
+![alt text](./images/image-9.png)
+
+- 검색창에 `ICon` 입력 : `Material Icon Theme` 설치
+
+![alt text](./images/image-10.png)
+
+- 검색창에 `live` 입력 : `Live Server` 설치
+
+## vsCode 세팅하기
+
+- `Ctrl + ,` 를 눌러 세팅 실행하기
+- Editor, Font Family : D2Coding 으로 변경하기
+
+- formatter 검색하여 `Prettier formatter`로 설정하기
+
+![alt text](./images/image-6.png)
+![alt text](./images/image-7.png)
+
+- save 검색하여 `format on Save` 체크하기
+
+![alt text](./images/image-8.png)
--- UI-2024-12-Join-Login/js/join.js
+++ UI-2024-12-Join-Login/js/join.js
@@ -1,15 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const join_body = document.querySelector("section.join.body");
   const join_form = join_body.querySelector("form");
-  const input_username = join_body.querySelector(
-    "input[name='username']"
-  );
-  const input_password = join_body.querySelector(
-    "input[name='password']"
-  );
-  const input_re_password = join_body.querySelector(
-    "input[name='re_password']"
-  );
+  const input_username = join_body.querySelector("input[name='username']");
+  const input_password = join_body.querySelector("input[name='password']");
+  const input_re_password = join_body.querySelector("input[name='re_password']");
   const input_name = join_body.querySelector("input[name='name']");
   const input_tel = join_body.querySelector("input[name='tel']");
   const btn_join = join_body.querySelector("input.join");
@@ -18,10 +12,39 @@ document.addEventListener("DOMContentLoaded", () => {
     const value_username = input_username.value;
     const value_password = input_password.value;
     const value_re_password = input_re_password.value;
+    const value_name = input_name.value;
+    const value_tel = input_tel.value;
+    if (!value_username) {
+      alert("USER ID를 입력해주세요.");
+      input_username.select();
+      return false;
+    }
+    if (!value_password) {
+      alert("비밀번호를 입력해주세요.");
+      input_password.select();
+      return false;
+    }
+
+    if (!value_re_password) {
+      alert("비밀번호 확인을 입력해주세요.");
+      input_re_password.select();
+      return false;
+    }
+
+    if (value_password !== value_re_password) {
+      alert("비밀번호가 일치하지 않습니다.");
+      input_re_password.select();
+      return false;
+    }
   };
   /*
   join(회원가입) 버튼을 클릭했을때
   1. username, password, re_password 가 입력되었는지 검사하기
   2. password 와 re_password 가 일치하는지 검사하기
   */
+
+  btn_join.addEventListener("click", (e) => {
+    // e.preventDefault();
+    onValidation();
+  });
 });
